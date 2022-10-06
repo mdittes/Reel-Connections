9b0cf7454e150ff5ec8bb699e3fb95688f11687e
Matt Dittes <mattdittes@gmail.com>
2022-10-06 11:37:18 -0400

    Frontend fix from merge in app/index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -28,4 +9,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
->>>>>>> a99f3a3c460dfae2b768235387a53059b84f89c3
