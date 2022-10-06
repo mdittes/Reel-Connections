e7623f85d8148a736abfb123cff6888649caf1f8
Sucram <marcuscrabtree4@gmail.com>
2022-10-06 11:42:40 -0400

    fixed frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavbarComp';
@@ -40,4 +13,3 @@ function App() {
 }
 
 export default App;
->>>>>>> a99f3a3c460dfae2b768235387a53059b84f89c3
